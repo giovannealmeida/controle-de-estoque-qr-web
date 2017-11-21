cdc46f88947ac9a8d830a80eb0148b5a77c45a4a
José Neto <jbsneto18@gmail.com>
2017-11-21 10:36:18 -0300

    Passando informações para o header

--- application/controllers/Equipe_controller.php
+++ application/controllers/Equipe_controller.php
@@ -15,10 +15,11 @@ class Equipe_controller extends CI_Controller {
     }
 
     public function loja() {
-      $this->load->view("_inc/header");
-      $this->load->view("_inc/menu");
-      $this->load->view("cadastro_loja");
-      $this->load->view("_inc/footer");
+        $data = $this->user_info;
+        $this->load->view("_inc/header", $data);
+        $this->load->view("_inc/menu");
+        $this->load->view("cadastro_loja");
+        $this->load->view("_inc/footer");
     }
 
 }
