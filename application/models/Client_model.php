6f881ae395917a126486dc56a0a88ec2a68888ff
José Neto <jbsneto18@gmail.com>
2018-03-09 11:09:49 -0300

    Retornando o id do clieente inseerido

--- application/models/Client_model.php
+++ application/models/Client_model.php
@@ -11,7 +11,7 @@ class Client_model extends CI_Model {
     public function insert($data) {
         $this->db->insert('tb_clients', $data);
         if ($this->db->affected_rows() == 1) {
-            return TRUE;
+            return $this->db->insert_id();
         }
 
         return FALSE;
